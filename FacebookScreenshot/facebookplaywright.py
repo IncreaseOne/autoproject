4ce54a192da9d33318cf9c7f1132943c93066654
wangyong <279761649@qq.com>
2023-07-04 16:25:25 +0800

    处理异步任务的事件循环

--- FacebookScreenshot/facebookplaywright.py
+++ FacebookScreenshot/facebookplaywright.py
@@ -69,6 +69,7 @@ class AutoScreenshot():
             chromium = playwright.chromium
             browser = await chromium.launch(
                 headless=True)
+                # proxy={"server": "http://127.0.0.1:19180"})
             context = await browser.new_context(storage_state=f"{BASE_DIR}/login_data_facebook.json", viewport={"width": 1080, "height": 1920})
             await self.screenshot(context)
             await context.close()
--- FacebookScreenshot/views.py
+++ FacebookScreenshot/views.py
@@ -17,7 +17,9 @@ class Facebook(APIView):
         if not code:
             return JsonResponse({"code": 400, "message": "参数传递异常"}, json_dumps_params={"ensure_ascii": False})
         screen_shot = AutoScreenshot(code=code)
-        results = asyncio.run(screen_shot.run())
+        loop = asyncio.new_event_loop()
+        asyncio.set_event_loop(loop)
+        results = loop.run_until_complete(screen_shot.run())
         if not results:
             return JsonResponse({"code": 400, "message": "折扣码无效"})
         return JsonResponse({"code": 200, "message":"成功", "data": [i for i in results]}, json_dumps_params={"ensure_ascii": False})
