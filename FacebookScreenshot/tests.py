47f37708ee20151a9e7c6e92dcd983a98b49d3f2
wangyong <279761649@qq.com>
2023-07-05 11:12:55 +0800

    wu

--- FacebookScreenshot/tests.py
+++ FacebookScreenshot/tests.py
@@ -7,7 +7,7 @@ async def run():
     async with async_playwright() as playwright:
         chromium = playwright.chromium  # or "firefox" or "webkit".
         browser = await chromium.launch(
-            headless=False)
+            headless=True)
         context = await browser.new_context()
         page = await context.new_page()
         await page.goto("https://www.baidu.com")
