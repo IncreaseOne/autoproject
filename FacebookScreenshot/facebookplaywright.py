92203aa25e04f4e5240854d9aff61dd88dcedd3e
wangyong <279761649@qq.com>
2023-07-19 15:17:04 +0800

    None

--- FacebookScreenshot/facebookplaywright.py
+++ FacebookScreenshot/facebookplaywright.py
@@ -37,7 +37,7 @@ class AutoScreenshot():
     async def screenshot(self, context):
 
         page = await context.new_page()
-        await page.goto(f'https://www.facebook.com/search/top/?q={self.code}', wait_until="domcontentloaded")
+        await page.goto(f'https://www.facebook.com/search/posts/?q={self.code}', wait_until="domcontentloaded")
         logging.info(f"{self.code}: 页面渲染完成")
         if await page.get_by_text("展开").count() > 0:
             expands = await page.get_by_text("展开").all()
@@ -46,7 +46,7 @@ class AutoScreenshot():
                     await i.click()
                 except:
                     pass
-        while await page.locator("a", has_text="了解更多").is_visible(timeout=3000) is False:
+        while await page.locator("span", has_text="已经到底啦~").is_visible(timeout=3000) is False:
             await page.mouse.wheel(0, 500)
             if await page.get_by_text("展开").count() > 0:
                 expands = await page.get_by_text("展开").all()
@@ -76,7 +76,7 @@ class AutoScreenshot():
         async with async_playwright() as playwright:
             chromium = playwright.chromium
             browser = await chromium.launch(
-                headless=True)
+                headless=False)
             now_time = time.time()
             with open(f"{BASE_DIR}/login_data_facebook.json", mode="r") as f:
                 obj = json.load(f)
@@ -96,7 +96,7 @@ class AutoScreenshot():
         async with async_playwright() as playwright:
             chromium = playwright.chromium
             browser = await chromium.launch(
-                headless=True)
+                headless=False)
             context = await browser.new_context()
             await self.login(context=context, account=self.account)
 
