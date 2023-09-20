06cc5f43b71b8214507a7dca364e0f592f6107b7
wangyong <279761649@qq.com>
2023-09-20 10:45:59 +0800

    None

--- FacebookScreenshot/tests.py
+++ FacebookScreenshot/tests.py
@@ -6,21 +6,6 @@ import time
 #
 from functools import reduce
 
-# from playwright.sync_api import Playwright, sync_playwright, expect
-#
-#
-# def run(playwright: Playwright) -> None:
-#     browser = playwright.chromium.launch(headless=False, proxy={"server": "http://127.0.0.1:19180"})
-#     context = browser.new_context()
-#     page = context.new_page()
-#     page.goto(r"https://www.facebook.com/search/posts/?q={self.search}")
-#     page.wait_for_timeout(100000)
-#     element = page.locator("span", has_text=re.compile(".*已经到底啦~.*|.*找不到任何结果.*|.*加入或登录 Facebook.*|.*你的帐户已锁定.*"))
-#     print(element.inner_html())
-#     context.close()
-#     browser.close()
-#
-# run(sync_playwright().start())
 
 
 
